7629fcec257001925f47961532096a9dda97daaa
Raul <raulmuresancalin@gmail.com>
2025-03-11 16:13:28 +0200

    Add Post component and enhance ForumPage layout with improved UI elements

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 
 import { FollowList } from "@/components/ui/FollowList";
+import { Post } from "@/components/ui/Post";
 import { useState } from "react";
 
 export default function ForumPage() {
     const [isOpen, setIsOpen] = useState(true);
+    const text1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     return (
         <div className="flex">
 
@@ -34,13 +36,20 @@ export default function ForumPage() {
             <main className={`transition-all duration-300 ease-in-out flex-1 p-6 ${isOpen ? "ml-[300px]" : "ml-[50px]"}`}>
                 {/* meniu add, search, filter*/}
                 <div className={`flex justify-around space-x-6 transition-all duration-300 ease-in-out flex-1 p-6 ${isOpen ? "ml-[300px]" : "ml-[50px]"}`}>
-                    <button className="px-6 bg-gray-400 rounded-md">+ Add</button>
-                    <button className="px-6 bg-gray-400 rounded-md">Search</button>
-                    <button className="px-6 bg-gray-400 rounded-md">Filter</button>
+                    <button className="px-6 bg-gray-400 rounded-md hover:bg-gray-300">+ Add</button>
+                    <input
+                        type="text"
+                        value="Search Box"
+                    />
+                    <button className="px-6 bg-gray-400 rounded-md hover:bg-gray-300">Search</button>
+                    <button className="px-6 bg-gray-400 rounded-md hover:bg-gray-300">Filter</button>
                 </div>
 
                 {/* postari */}
                 {/* <h1 className="text-3xl font-bold"></h1> */}
+                <div>
+                    <Post id={5} userId={3} title="Postarea nr 1" author="User12341" text={text1} status={2} tag="C" timestamp="11 Mar 2025" score={5} />
+                </div>
                 <div className="mt-4 space-y-4">
                     <div className="p-4 bg-white shadow-md rounded-lg">
                         <h2 className="text-xl font-semibold">Postare 1</h2>
--- components/ui/Post.tsx
+++ components/ui/Post.tsx
@@ -1,22 +1,48 @@
 "use client"
 
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
+import { Button } from "./button";
+
 interface PostProps {
     id: number;
     userId: number;
     title: string;
     author: string;
+    text: string;
     status: number; //0="Sent" | 1="In progress" | 2="Solved";
     tag: string;
+    timestamp: string;
     score: number;
     image?: string;
 }
 
-export const Post = ({ id, userId, title, author, status, tag, score, image }: PostProps) => {
+export const Post = ({ id, userId, title, author, text, status, tag, timestamp, score, image }: PostProps) => {
     return (
-        <div>
-            
-            <h2 className="text-bold text-lg">{title}</h2>
-            
+        <div className="p-4 bg-white shadow-md rounded-lg">
+            <div className="flex justify-between">
+                <h1>{author}</h1>
+                {status === 0 && (
+                    <h1>Sent</h1>
+                )}
+                {status === 1 && (
+                    <h1>In progress</h1>
+                )}
+                {status === 2 && (
+                    <h1>Solved</h1>
+                )}
+                <h1>{timestamp}</h1>
+            </div>
+            <h2 className="text-bold text-3xl">{title}</h2>
+            <p>{text}</p>
+            <div className="flex justify-between">
+                <div className="p-5">
+                    <button><FaArrowUp /></button>
+                    <label>{score}</label>
+                    <button><FaArrowDown /></button>
+                </div>
+                <Button>Comments</Button>
+                <button>⠇</button>
+            </div>
         </div>
     )
 }
